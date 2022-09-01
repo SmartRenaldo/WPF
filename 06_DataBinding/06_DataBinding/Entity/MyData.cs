258cc04a166a60e90a8487d9b81606371dfaa74d
correctgavin@gmail.com <correctgavin@gmail.com>
2022-09-01 16:04:18 +0930

    data binding

---
+++ 06_DataBinding/06_DataBinding/Entity/MyData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_DataBinding.Entity
+{
+    class MyData
+    {
+        private string color = "Red";
+
+        public string Color 
+        {
+            get  => color; 
+            set => color = value;
+        }
+    }
+}
