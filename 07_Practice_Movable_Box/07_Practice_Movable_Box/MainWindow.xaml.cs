2ab5e26510140fa16c2349afa757a6e02891edbe
correctgavin@gmail.com <correctgavin@gmail.com>
2022-09-01 18:04:25 +0930

    data binding

--- 07_Practice_Movable_Box/07_Practice_Movable_Box/MainWindow.xaml.cs
+++ 07_Practice_Movable_Box/07_Practice_Movable_Box/MainWindow.xaml.cs
@@ -27,7 +27,49 @@ namespace _07_Practice_Movable_Box
 
         private void Grid_KeyDown(object sender, KeyEventArgs e)
         {
+            UIElementCollection children = gridEle.Children;
+            Border border = null;
 
+            for (int i = 0; i < children.Count; i++)
+            {
+                if (children[i] is Border)
+                {
+                    if (((children[i] as Border).Background as SolidColorBrush).Color.Equals(Colors.White))
+                    {
+                        border = children[i] as Border;
+                    }
+
+                    (children[i] as Border).Background = new SolidColorBrush(Colors.Transparent);
+                }
+            }
+
+            string name = border.Name;
+            int index = Convert.ToInt32(name.Replace("b", ""));
+
+            switch (e.Key)
+            {
+                case Key.Up:
+                    index = index - 3 > 0 ? index - 3 : index;
+                    break;
+                case Key.Left:
+                    index = index - 1 > 0 ? index - 1 : index;
+                    break;
+                case Key.Down:
+                    index = index + 3 < 10 ? index + 3 : index;
+                    break;
+                case Key.Right:
+                    index = index + 1 < 10 ? index + 1 : index;
+                    break;
+                default:
+                    break;
+            }
+
+            object control =  gridEle.FindName("b" + index);
+
+            if (control != null)
+            {
+                (control as Border).Background = new SolidColorBrush(Colors.White);
+            }
         }
     }
 }
