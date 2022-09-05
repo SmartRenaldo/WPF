0d7a4c3d8e980b0936370eef680931c5418ecbea
correctgavin@gmail.com <correctgavin@gmail.com>
2022-09-05 11:22:38 +0930

    combobox

---
+++ 14_ComboBox/14_ComboBox/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _14_ComboBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //1. use items
+            /*this.comboBox1.Items.Add("c");
+            this.comboBox1.Items.Add("i");
+            this.comboBox1.Items.Add("m");
+            this.comboBox1.Items.Add("s");*/
+
+            //2. use DataContext
+            List<NewsInfo> list = new List<NewsInfo>();
+            list.Add(new NewsInfo() { Name = "BBC", Description = "BBC NEWS" });
+            list.Add(new NewsInfo() { Name = "CNN", Description = "CNN NEWS" });
+            list.Add(new NewsInfo() { Name = "FBI", Description = "FBI NEWS" });
+
+            this.comboBox1.DataContext = list;
+            this.comboBox1.DisplayMemberPath = "Name";
+            this.comboBox1.SelectedValuePath = "Description";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            List<NewsInfo> lists = this.comboBox1.ItemsSource as List<NewsInfo>;
+            lists.RemoveAt(0);
+            this.comboBox1.ItemsSource = lists;
+        }
+    }
+}
---
+++ 14_ComboBox/14_ComboBox/VacationSpots.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_ComboBox
+{
+    class VacationSpots : ObservableCollection<string>
+    {
+        public VacationSpots()
+        {
+
+            Add("Spain");
+            Add("France");
+            Add("Japanese");
+            Add("Mexico");
+            Add("Italy");
+            Add("Chinese");
+        }
+    }
+}
