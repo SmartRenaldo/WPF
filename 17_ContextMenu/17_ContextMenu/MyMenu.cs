1320e343b44999b9e95741009b1486622ac8d0fc
correctgavin@gmail.com <correctgavin@gmail.com>
2022-09-05 14:27:44 +0930

    17_ContextMenu

---
+++ 17_ContextMenu/17_ContextMenu/MyMenu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_ContextMenu
+{
+    public class MyMenu
+    {
+        public string Name { get; set; }
+        public List<MyMenu> SubMenu { get; set; }
+
+        public MyMenu (string name, MyMenu parentMenu)
+        {
+            Name = name;
+            
+            if (parentMenu != null)
+            {
+                List<MyMenu> list = parentMenu.SubMenu ?? new List<MyMenu>();
+                list.Add (this);
+                parentMenu.SubMenu = list;
+            }
+        }
+    }
+}
