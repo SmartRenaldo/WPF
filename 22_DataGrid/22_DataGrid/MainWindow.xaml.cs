f3c2b444ca9e4253f492c19f15795a1e3da0a696
correctgavin@gmail.com <correctgavin@gmail.com>
2022-09-06 09:51:24 +0930

    DataGrid

---
+++ 22_DataGrid/22_DataGrid/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _22_DataGrid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<UserInfo> list = new List<UserInfo>();
+            list.Add(new UserInfo() { Id = 0, Age = 26, Name = "Shanne", DeptId = 0 });
+            list.Add(new UserInfo() { Id = 1, Age = 22, Name = "Gourge", DeptId = 0 });
+            list.Add(new UserInfo() { Id = 2, Age = 28, Name = "Snake", DeptId = 1 });
+            list.Add(new UserInfo() { Id = 3, Age = 39, Name = "Albert", DeptId = 1 });
+
+            List<DepartmentInfo> departmentList = new List<DepartmentInfo>();
+            departmentList.Add(new DepartmentInfo() { Id = 0, Name = "Technique Apartment" });
+            departmentList.Add(new DepartmentInfo() { Id = 1, Name = "Marketing Apartment" });
+
+            this.depts.ItemsSource = departmentList;
+            this.depts.DisplayMemberPath = "Name";
+            this.depts.SelectedValuePath = "Id";
+
+            //this.awesomeGrid.DataContext = list;
+            this.awesomeGrid.ItemsSource = list;
+
+            ICollectionView task = CollectionViewSource.GetDefaultView(awesomeGrid.ItemsSource);
+
+            if (task != null && task.CanSort == true)
+            {
+                task.SortDescriptions.Clear();
+                task.SortDescriptions.Add(new SortDescription("Age", ListSortDirection.Ascending));
+            }
+        }
+    }
+}
---
+++ 22_DataGrid/22_DataGrid/UserInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22_DataGrid
+{
+    public class UserInfo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int DeptId { get; set; }
+    }
+}
