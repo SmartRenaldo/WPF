c83a633e1b3d34edbf39e4bb4f878c1deda728e5
correctgavin@gmail.com <correctgavin@gmail.com>
2022-09-06 09:50:56 +0930

    TreeView

---
+++ 23_TreeView/23_TreeView/MenuGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_TreeView
+{
+    public class MenuGenerator
+    {
+        public static List<MyMenu> CreateMenus()
+        {
+            List<MyMenu> menuList = new List<MyMenu>();
+
+            MyMenu m1 = new MyMenu("Beef", null);
+            MyMenu m2 = new MyMenu("Pork", null);
+            MyMenu m3 = new MyMenu("Lamb", null);
+
+            MyMenu m3_1 = new MyMenu("BBQ", m3);
+            MyMenu m3_2 = new MyMenu("Roast", m3);
+
+            menuList.Add(m1);
+            menuList.Add(m2);
+            menuList.Add(m3);
+
+            return menuList;
+        }
+    }
+}
