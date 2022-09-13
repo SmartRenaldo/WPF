bdf08252b3cb7af377804a0e6ddcf74b7e358f1c
correctgavin@gmail.com <correctgavin@gmail.com>
2022-09-13 10:35:08 +0930

    ListView

--- 34_ListView/34_ListView/MainWindow.xaml.cs
+++ 34_ListView/34_ListView/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,40 @@ namespace _34_ListView
     /// </summary>
     public partial class MainWindow : Window
     {
+        SqlConnection sqlConnection;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            string conncetionString = ConfigurationManager.ConnectionStrings["_34_ListView.Properties.Settings.TestingDatabaseConnectionString"].ConnectionString;
+            sqlConnection = new SqlConnection(conncetionString);
+            ShowPatients();
         }
+
+        private void ShowPatients()
+        {
+            try
+            {
+                string query = "select * from Patient";
+                SqlDataAdapter adapter = new SqlDataAdapter(query, sqlConnection);
+
+                using (adapter)
+                {
+                    DataTable patientTable = new DataTable();
+
+                    adapter.Fill(patientTable);
+
+                    /*patients.DisplayMemberPath = "Name";
+                    patients.SelectedValuePath = "Id";*/
+                    patients.ItemsSource = patientTable.DefaultView;
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
     }
 }
