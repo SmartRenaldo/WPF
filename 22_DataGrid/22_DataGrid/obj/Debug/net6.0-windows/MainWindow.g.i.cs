2b2b233de4602d5c08cb533a9fced718288d17cb
correctgavin@gmail.com <correctgavin@gmail.com>
2022-09-13 09:54:10 +0930

    listview

--- 22_DataGrid/22_DataGrid/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ 22_DataGrid/22_DataGrid/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -69,7 +69,7 @@ namespace _22_DataGrid {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/22_DataGrid;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/22_DataGrid;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
